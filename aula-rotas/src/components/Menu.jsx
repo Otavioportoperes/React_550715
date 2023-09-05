f38d1210aee7ff6c6b30c098556afda125ab0732
otavioporto <RM550715@fiap.com.br>
2023-09-04 21:12:53 -0300

    corrigindo erros

--- aula-rotas/src/components/Menu.jsx
+++ aula-rotas/src/components/Menu.jsx
@@ -1,12 +1,12 @@
-import { Link } from "react-router-dom";
+import { Link } from "react-router-dom"
 
 export default function Menu(){
 
-    return
+    return(
     <nav className="menu">
              <link to='/'>JHome</link>
             <span>  </span>
             <Link to='/produtos'>Produtos</Link>
         </nav>
-    
+    )
 }
