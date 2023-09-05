8b9600a35247a4fda9757ed7a46dda5d98955896
otavioporto <RM550715@fiap.com.br>
2023-09-04 21:00:47 -0300

    Colocando os métodos no código

--- aula-rotas/src/main.jsx
+++ aula-rotas/src/main.jsx
@@ -14,3 +14,30 @@ import Home from './routes/Home/index.jsx';
 import Produtos from './routes/Produtos/index.jsx';
 import Error from './routes/Error/index.jsx';
 import EditarProdutos from './routes/EditarProdutos/index.jsx';
+
+//utilizando o método 'createBrowserRouter' cada objeto é uma das rotas
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App/>,
+    errorElement: <Error/>,
+    children:[
+      {
+        path: "/",
+        element:<Home/>
+      },
+      {
+        path: "/produtos",
+        element: <Produtos/>
+      },
+      {
+        path: "/editar/produtos/:id",
+        element: <EditarProdutos/>
+      }
+    ]
+  
+  }
+  ])
+  
+
+  
