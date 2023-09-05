78be687e5998c69b4ce3d3ef6b34ba0e664f3634
otavioporto <RM550715@fiap.com.br>
2023-09-04 21:45:59 -0300

    Atualizando as jsx

---
+++ aula-rotas/src/components/Produtos.jsx
@@ -0,0 +1,42 @@
+import { Link } from "react-router-dom";
+import { listaProdutos } from "../../../ListaProdutos";
+
+
+
+    export default function Produtos(){
+
+        return(
+            <main>
+                <h1>Produtos</h1>
+                {listaProdutos.map(prod => (
+                    <div key={prod.id}>
+                        <Link to={'/produtos/editar/${prod.id}'}>
+                            Editar o produto: {prod.nome}
+                            </Link>
+                        </div>
+                ))}
+            </main>
+)
+}
+
+
+               
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+
+
+
+
