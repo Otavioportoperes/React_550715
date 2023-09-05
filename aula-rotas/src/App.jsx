1e3eafb4c18ab2f96d64c6651d72cbad6abcca93
otavioporto <RM550715@fiap.com.br>
2023-09-04 21:20:48 -0300

    Importando pra app

--- aula-rotas/src/App.jsx
+++ aula-rotas/src/App.jsx
@@ -2,6 +2,9 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import { Outlet } from 'react-router-dom'
+import Menu from './components/Menu'
+import Rodape from './components/Rodape'
 
 function App() {
   const [count, setCount] = useState(0)
