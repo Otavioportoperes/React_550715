dfc1c6ea220907dedbf6f9d4e9c630a0dd7dd461
otavioporto <RM550715@fiap.com.br>
2023-09-04 22:01:12 -0300

    Atualizando o código

---
+++ aula-rotas/src/components/EditarProdutos.jsx
@@ -0,0 +1,28 @@
+//Recebendo o Id do produto, usando o useNavigate e UseParams
+import { useParams,useNavigate } from "react-router-dom";
+import { listaProdutos } from "./ListaProdutos";
+
+export default function EditarProduto(){
+
+    const lista = listaProdutos
+    const navegacao = useNavigate()
+    const {id} = useParams()
+
+    const proc = lista.filter(prod => prod.id == id)
+    const produto = proc[0]
+
+    const salvar = ()=>{
+        alert('Produto: ${produto.nome} editado com sucesso!')
+        return navegacao('/produtos')
+    }
+    return(
+        <main>
+            <h1>Editando o produto</h1>
+            <p>Editando os dados do produto: {produto.nome}</p>
+            <button onClick={salvar}>Salvar</button>
+        </main>
+    ) 
+    
+    
+    
+}
