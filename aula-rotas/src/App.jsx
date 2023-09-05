7957dd7ef34e3287626bcf53d28e4a61abc17b4a
otavioporto <RM550715@fiap.com.br>
2023-09-04 21:22:45 -0300

    Criando a Lista produtos

--- aula-rotas/src/App.jsx
+++ aula-rotas/src/App.jsx
@@ -6,6 +6,8 @@ import { Outlet } from 'react-router-dom'
 import Menu from './components/Menu'
 import Rodape from './components/Rodape'
 
+
+
 function App() {
   const [count, setCount] = useState(0)
 
