77c6a21e16397d92a9526151b34b06b7d364ada4
otavioporto <RM550715@fiap.com.br>
2023-09-04 21:12:19 -0300

    Configurando o Menu.jsx

--- aula-rotas/src/components/Menu.jsx
+++ aula-rotas/src/components/Menu.jsx
@@ -1,1 +1,12 @@
-impor
+import { Link } from "react-router-dom";
+
+export default function Menu(){
+
+    return
+    <nav className="menu">
+             <link to='/'>JHome</link>
+            <span>  </span>
+            <Link to='/produtos'>Produtos</Link>
+        </nav>
+    
+}
